3a37a4baeaf444c6e6402c5abed0cbe84011ebea
Yaroslav <stefanyshynyaroslavg@gmail.com>
2016-09-01 14:02:50 +0000

    add new file

---
+++ index.js
@@ -0,0 +1,24 @@
+// JavaScript File
+var num = Number(prompt('Enter val a'));
+
+    fibonacci(num);
+    
+    function fibonacci(num){
+    var fib = [0, 1];
+    for(var i=fib.length; i<=num; i++) {
+    
+         fib[i] = fib[i-2] + fib[i-1];
+    }
+    document.write(fib[num]);
+}
+
+
+/*document.write(fibonacci(num))
+
+function fibonacci(num){
+   return num < 1 ? 0 
+        : num <= 2 ? 1
+        : fibonacci(num - 1) + fibonacci(num - 2);
+}*/
+
+
